15b387a2bc9f5436839966296e502b19abc6de68
José Adolfo Galdámez <josegalre@pac-rom.com>
2015-12-08 17:30:26 -0600

    set custom changes

--- afyon_prod.sh
+++ afyon_prod.sh
@@ -1,2 +1,21 @@
-make -j64 ARCH=arm CROSS_COMPILE=~/COMMON_WS_FOR_ALL_8x26/MAIN/android/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi- msm8226-sec_defconfig VARIANT_DEFCONFIG=msm8926-sec_afyonltetmo_defconfig
-make -j64 ARCH=arm CROSS_COMPILE=~/COMMON_WS_FOR_ALL_8x26/MAIN/android/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi- 
+#!/bin/bash
+
+OUT_DIR="$(pwd)/OUT"
+
+rm -rf $OUT_DIR
+mkdir -p $OUT_DIR/kernel
+mkdir -p $OUT_DIR/modules
+ 
+echo " Cleaning kernel (make mrproper)..."
+make mrproper
+
+make -j4 ARCH=arm CROSS_COMPILE=/home/josegalre/Android/cm-11.0/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi- afyonlte_defconfig
+make -j4 ARCH=arm CROSS_COMPILE=/home/josegalre/Android/cm-11.0/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
+
+echo " Copying kernel (zImage) to $OUT_DIR/kernel/..."
+cp arch/arm/boot/zImage $OUT_DIR/kernel/
+
+echo " Copying modules (*.ko) to $OUT_DIR/modules/..."
+find . -name \*.ko -exec cp '{}' $OUT_DIR/modules/ ';'
+
+echo " All done!..."
